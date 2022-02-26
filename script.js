9f8d41b7c35c65851f978eb3b3533d91bda2cfbe
mazedur1056 <mazedur15-1056@diu.edu.bd>
2022-02-26 10:08:59 +0600

    commited

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 function validation() {
-    let x = document.forms["survey"]["firstName"].value;
-    if (isNaN(x)) {
-        text = "Input not valid";
+    let firstName = document.forms["survey"]["firstName"].value;
+    let v = 0;
+    if (firstName == "") {
+        document.getElementById("firstNameMsg").innerHTML = "Enter your first name";
     } else {
-    text = "Input OK";
+        document.getElementById("firstNameMsg").innerHTML = "";
+        v = v+1;
     }
-    document.getElementById("firstNameMsg").innerHTML = text;
+
+    let lastName = document.forms["survey"]["lastName"].value;
+    if (lastName == "") {
+        document.getElementById("lastNameMsg").innerHTML = "Enter your last name";
+    } else {
+        document.getElementById("lastNameMsg").innerHTML = "";
+        v = v+1;
+    }
+
+    let email = document.forms["survey"]["email"].value;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(mailformat)) {
+        document.getElementById("emailMsg").innerHTML = "";
+        v = v+1;
+    } else {
+        document.getElementById("emailMsg").innerHTML = "Enter a valid email";
+    }
+
+    let date = new Date(document.forms["survey"]["date"].value);
+    var today = new Date();
+    if (date >= today || date == "Invalid Date") {
+        document.getElementById("dateMsg").innerHTML = "Invalid birth date"
+    } else {
+        document.getElementById("dateMsg").innerHTML = "";
+        v = v+1;
+    }
+
+    if (v == 4) {
+        alert("Successfully submitted");
+    }
 }
